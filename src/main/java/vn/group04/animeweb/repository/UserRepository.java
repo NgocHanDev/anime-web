94e17062874c3d31031fef63234aeb75c3d4c9ee
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2023-12-08 23:21:35 +0700

    nothing

--- src/main/java/vn/group04/animeweb/repository/UserRepository.java
+++ src/main/java/vn/group04/animeweb/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package vn.group04.animeweb.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import vn.group04.animeweb.entity.User;
@@ -10,4 +12,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     public User findByEmail(String email);
     public boolean existsByUserName(String userName);
     public boolean existsByEmail(String email);
+    public Page<User> findAllByFullNameContainingIgnoreCaseOrEmailContainingIgnoreCaseOrUserNameContainingIgnoreCase(String fullName, String email, String userName, Pageable pageable);
 }
