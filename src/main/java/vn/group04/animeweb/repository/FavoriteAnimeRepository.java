ffcfe85fdbe0ff1e09dd06b7cc3b3252c648c6ed
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2024-01-07 22:36:19 +0700

    tìm anime yêu thích

--- src/main/java/vn/group04/animeweb/repository/FavoriteAnimeRepository.java
+++ src/main/java/vn/group04/animeweb/repository/FavoriteAnimeRepository.java
@@ -1,8 +1,9 @@
 package vn.group04.animeweb.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.transaction.annotation.Transactional;
 import vn.group04.animeweb.entity.FavoriteAnime;
 
 import java.util.Optional;
@@ -10,4 +11,5 @@ import java.util.Optional;
 @RepositoryRestResource(path = "danh-sach-yeu-thich")
 public interface FavoriteAnimeRepository extends JpaRepository<FavoriteAnime, Integer> {
     public FavoriteAnime findByAnime_IdAndUser_Id(int animeId, int userId);
+    public Page<FavoriteAnime> findAllByUser_Id(int userId, Pageable pageable);
 }
