46d698f0262858fa1fa9f2a97f96801ffde57332
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2024-02-01 18:46:25 +0700

    cập nhật hai api liên quan đến favoriteanime

--- src/main/java/vn/group04/animeweb/repository/FavoriteAnimeRepository.java
+++ src/main/java/vn/group04/animeweb/repository/FavoriteAnimeRepository.java
@@ -12,4 +12,6 @@ import java.util.Optional;
 public interface FavoriteAnimeRepository extends JpaRepository<FavoriteAnime, Integer> {
     public FavoriteAnime findByAnime_IdAndUser_Id(int animeId, int userId);
     public Page<FavoriteAnime> findAllByUser_Id(int userId, Pageable pageable);
+    public boolean existsByAnime_IdAndUser_Id(int animeId, int userId);
+    public int countAllByAnime_IdAndUser_Id(int animeId, int userId);
 }
